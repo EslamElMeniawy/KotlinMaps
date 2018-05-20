7180e61dda25525a422fbf50ef2718195572cc63
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2018-05-20 10:56:54 +0200

    Add marker image and snippet

--- app/src/main/java/elmeniawy/eslam/pockemon/MapsActivity.kt
+++ app/src/main/java/elmeniawy/eslam/pockemon/MapsActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -17,6 +18,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
@@ -37,7 +39,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.addMarker(
+                MarkerOptions()
+                        .position(sydney)
+                        .title("Me")
+                        .snippet("This is my location")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario))
+        )
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14f))
     }
 }
